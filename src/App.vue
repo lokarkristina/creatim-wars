a4f4aa217020a8bc9243e100e5f8a15dc564e3a4
Kristina Lokar <lokarkristina79@gmail.com>
2025-03-27 11:13:19 +0100

    CREA-25 | Build the header.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import AppHeader from '@/components/AppHeader.vue'
 </script>
 
 <template>
-  <header>Header.</header>
+  <AppHeader />
 
   <main role="main">
     <RouterView />
